f1cdbe2aa70c057a7bb33067f98fc94ab2415666
Vinicius Dias <vinicius-mdias@hotmail.com>
2020-11-26 13:25:45 -0300

    Create a Loading component

--- src/components/ShowClimate/index.tsx
+++ src/components/ShowClimate/index.tsx
@@ -29,10 +29,12 @@ const ShowClimate: React.FC<LocationProps> = ({
   city,
   state,
 }: LocationProps) => {
+  const [loading, setLoading] = useState(false);
   const [weathers, setWeathers] = useState<ClimateProps[]>([]);
 
   /* Request when input is clicked */
   useEffect(() => {
+    setLoading(true);
     openWeatherApi
       .get(
         `/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,daily&units=metric&appid=52219e46da1db33cf0ad6c6b4cb4d908`,
@@ -40,54 +42,65 @@ const ShowClimate: React.FC<LocationProps> = ({
       .then((response) => {
         setWeathers(response.data.hourly.slice(0, 6));
       });
+    setLoading(false);
   }, [latitude, longitude]);
 
   return (
     <Container>
-      <h1>
-        {weathers
-          .slice(0, 1)
-          .map(
-            (weather) =>
-              `${weather.weather[0].description} currently in ${city} in ${state}. The temperature is ${weather.temp} °C`,
-          )}
-      </h1>
-      <div>
-        <ul>
-          <li />
-          <li>Condition</li>
-          <li>Temperature</li>
-          <li>Feels Like</li>
-          <li>Humidity</li>
-        </ul>
-
-        {weathers.map((weather) => (
-          <ul key={weather.dt}>
-            {/* Am/Pm logical */}
-            <li>
-              {(weather.hour = new Date(weather.dt * 1000).getHours()) - 12 < 0
-                ? ((weather.hour = new Date(weather.dt * 1000).getHours()) -
-                    12) *
-                  -1
-                : (weather.hour = new Date(weather.dt * 1000).getHours()) - 12}
-
-              {(weather.hour = new Date(weather.dt * 1000).getHours()) - 12 <
-              0 ? (
-                <span>:00 am</span>
-              ) : (
-                <span>:00 pm</span>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <h1>
+            {weathers
+              .slice(0, 1)
+              .map(
+                (weather) =>
+                  `${weather.weather[0].description} currently in ${city} in ${state}. The temperature is ${weather.temp} °C`,
               )}
-            </li>
-            <img
-              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-              alt="Weather icons based on temperature"
-            />
-            <li>{weather.temp} °C</li>
-            <li>{weather.feels_like} °C</li>
-            <li>{weather.humidity} %</li>
-          </ul>
-        ))}
-      </div>
+          </h1>
+          <div>
+            <ul>
+              <li />
+              <li>Condition</li>
+              <li>Temperature</li>
+              <li>Feels Like</li>
+              <li>Humidity</li>
+            </ul>
+
+            {weathers.map((weather) => (
+              <ul key={weather.dt}>
+                {/* Am/Pm logical */}
+                <li>
+                  {(weather.hour = new Date(weather.dt * 1000).getHours()) -
+                    12 <
+                  0
+                    ? ((weather.hour = new Date(weather.dt * 1000).getHours()) -
+                        12) *
+                      -1
+                    : (weather.hour = new Date(weather.dt * 1000).getHours()) -
+                      12}
+
+                  {(weather.hour = new Date(weather.dt * 1000).getHours()) -
+                    12 <
+                  0 ? (
+                    <span>:00 am</span>
+                  ) : (
+                    <span>:00 pm</span>
+                  )}
+                </li>
+                <img
+                  src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  alt="Weather icons based on temperature"
+                />
+                <li>{weather.temp} °C</li>
+                <li>{weather.feels_like} °C</li>
+                <li>{weather.humidity} %</li>
+              </ul>
+            ))}
+          </div>
+        </>
+      )}
     </Container>
   );
 };
